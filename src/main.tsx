65c9e09311c5e0e4025b2e50851865d58dbb1bac
lastknighcoder <2223106858@qq.com>
2023-04-28 18:55:35 +0800

    init

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// import Paragraph from "./Components/Paragraph";
+import CompositionStart from "./demos/CompositionStart";
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <CompositionStart />
+  </React.StrictMode>,
+)
