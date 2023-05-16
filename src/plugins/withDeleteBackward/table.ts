a4886a6a838bd51ce86f64329f87f8a0fdc61cd6
lastknighcoder <2223106858@qq.com>
2023-05-16 12:56:24 +0800

    feat: 表格删除优化

--- src/plugins/withDeleteBackward/table.ts
+++ src/plugins/withDeleteBackward/table.ts
@@ -1,5 +1,6 @@
-import {Editor, Range} from "slate";
-import {movePrevCol} from "../../utils";
+import {Editor, Element as SlateElement, Range, Transforms} from "slate";
+import {isAtParagraphStart, isParagraphElement, isParagraphEmpty, movePrevCol} from "../../utils";
+import { ParagraphElement } from "../../custom-types";
 
 const table = (editor: Editor) => {
   const { deleteBackward } = editor;
@@ -18,6 +19,28 @@ const table = (editor: Editor) => {
           const [, path] = text;
           if (path[path.length - 1] === 0 && Editor.isStart(editor, selection.anchor, path)) {
             movePrevCol(editor);
+            return;
+          }
+        }
+      }
+      if (isAtParagraphStart(editor)) {
+        const [match] = Editor.nodes(editor, {
+          match: n => SlateElement.isElement(n) && isParagraphElement(n),
+        });
+        const [node, path] = match;
+        // 如果前一个是 code-block，删除当前 paragraph，将光标移动到 code-block 的末尾
+        const prevPath = Editor.before(editor, path);
+        if (prevPath) {
+          const [prevMatch] = Editor.nodes(editor, {
+            at: prevPath,
+            match: n => SlateElement.isElement(n) && n.type === 'table',
+          });
+          if (prevMatch) {
+            if (isParagraphEmpty(node as ParagraphElement)) {
+              Transforms.removeNodes(editor, { at: path });
+            }
+            console.log('prevMatch', prevMatch);
+            Transforms.move(editor, { distance: -1, unit: 'line' });
             return;
           }
         }
