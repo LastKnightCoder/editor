39575d813c20e7c50d219d0baf958035e6b7f655
lastknighcoder <2223106858@qq.com>
2023-07-03 22:18:44 +0800

    上下移动时让选项进入到视野中

--- src/pages/Editor/components/BlockPanel/Item/index.tsx
+++ src/pages/Editor/components/BlockPanel/Item/index.tsx
@@ -1,6 +1,7 @@
 import classnames from 'classnames';
 import styles from './index.module.less';
 import {IBlockPanelListItem} from "../../../types/blockPanel.ts";
+import {useEffect, useRef} from "react";
 
 interface IItemProps {
   showBottomLine: boolean;
@@ -10,11 +11,21 @@ interface IItemProps {
 
 const Item = (props: IItemProps) => {
   const { showBottomLine, item, active } = props;
+  const ref = useRef<HTMLDivElement>(null);
 
-
+  useEffect(() => {
+    if (active) {
+      // 滚动到可视区域
+      ref.current?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'start',
+      });
+    }
+  }, [active]);
 
   return (
-    <div className={classnames(styles.item, { [styles.active]: active })}>
+    <div ref={ref} className={classnames(styles.item, { [styles.active]: active })}>
       <div className={styles.content}>
         {item.title}
       </div>
--- src/pages/Editor/configs/blockPanel.ts
+++ src/pages/Editor/configs/blockPanel.ts
@@ -1,5 +1,15 @@
 import {IBlockPanelListItem} from "../types/blockPanel.ts";
-import {insertHeader} from "@/pages/Editor/utils";
+import {
+  insertHeader,
+  insertCallout,
+  insertCodeBlock,
+  insertBlockMath,
+  insertTable,
+  insertMermaid,
+  insertBulletList,
+  insertCheckList,
+  insertImage
+} from "../utils";
 
 export const blockPanelList: IBlockPanelListItem[] = [{
   icon: 'h1',
@@ -16,5 +26,111 @@ export const blockPanelList: IBlockPanelListItem[] = [{
   description: '二级标题',
   onClick: (editor) => {
     insertHeader(editor, 2);
+  }
+}, {
+  icon: 'callout',
+  title: '提示',
+  keywords: ['callout', '提示'],
+  description: '提示',
+  onClick: (editor) => {
+    insertCallout(editor, 'tip');
+  }
+}, {
+  icon: 'callout',
+  title: '信息',
+  keywords: ['callout', '信息'],
+  description: '信息',
+  onClick: (editor) => {
+    insertCallout(editor, 'info');
+  }
+}, {
+  icon: 'callout',
+  title: '笔记',
+  keywords: ['callout', '笔记'],
+  description: '笔记',
+  onClick: (editor) => {
+    insertCallout(editor, 'note');
+  }
+}, {
+  icon: 'callout',
+  title: '危险',
+  keywords: ['callout', '危险'],
+  description: '危险',
+  onClick: (editor) => {
+    insertCallout(editor, 'danger');
+  }
+}, {
+  icon: 'callout',
+  title: '警告',
+  keywords: ['callout', '警告'],
+  description: '警告',
+  onClick: (editor) => {
+    insertCallout(editor, 'warning');
+  }
+}, {
+  icon: 'codeblock',
+  title: '代码块',
+  keywords: ['codeblock', '代码块'],
+  description: '代码块',
+  onClick: (editor) => {
+    insertCodeBlock(editor);
+  }
+}, {
+  icon: 'math',
+  title: '数学公式',
+  keywords: ['math', '数学公式'],
+  description: '数学公式',
+  onClick: (editor) => {
+    insertBlockMath(editor);
+  }
+}, {
+  icon: 'table',
+  title: '表格',
+  keywords: ['table', '表格'],
+  description: '表格',
+  onClick: (editor) => {
+    insertTable(editor, 3, 3);
+  }
+}, {
+  icon: 'mermaid',
+  title: '流程图',
+  keywords: ['mermaid', '流程图'],
+  description: '流程图',
+  onClick: (editor) => {
+    insertMermaid(editor);
+  }
+}, {
+  icon: 'bulleted-list',
+  title: '无序列表',
+  keywords: ['bulleted-list', '列表'],
+  description: '无序列表',
+  onClick: (editor) => {
+    insertBulletList(editor);
+  }
+}, {
+  icon: 'numbered-list',
+  title: '有序列表',
+  keywords: ['numbered-list', '列表'],
+  description: '有序列表',
+  onClick: (editor) => {
+    insertBulletList(editor);
+  }
+}, {
+  icon: 'check-list',
+  title: '任务列表',
+  keywords: ['check-list', '列表'],
+  description: '任务列表',
+  onClick: (editor) => {
+    insertCheckList(editor);
+  }
+}, {
+  icon: 'image',
+  title: '图片',
+  keywords: ['image', '图片'],
+  description: '图片',
+  onClick: (editor) => {
+    insertImage(editor, {
+      url: ''
+    })
   }
 }];
