46b1d705a655c778a6920f695cbd2c54bcb8a5ff
lastknighcoder <2223106858@qq.com>
2023-09-07 09:53:52 +0800

    feat: Callout 标题可以编辑

--- src/components/Editor/components/Callout/index.tsx
+++ src/components/Editor/components/Callout/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import classnames from 'classnames';
 import styles from './index.module.less';
-import { RenderElementProps } from "slate-react";
+import { RenderElementProps, useSlate, ReactEditor } from "slate-react";
 import { CalloutElement } from "../../types";
 import AddParagraph from "../AddParagraph";
+import {Transforms} from "slate";
 
 interface ICalloutProps {
   attributes: RenderElementProps['attributes'];
@@ -32,12 +33,37 @@ const Callout: React.FC<React.PropsWithChildren<ICalloutProps>> = (props) => {
   const { attributes, element, children } = props;
   const { calloutType, title } = element;
   const { title: defaultTitle } = configs[calloutType];
+  const editor = useSlate();
+  const [realTitle, setRealTitle] = useState<string>(title || defaultTitle);
+  const titleRef = useRef<HTMLParagraphElement>(null);
+
+  const handleTitleBlur = () => {
+    const path = ReactEditor.findPath(editor, element);
+    Transforms.setNodes(editor, { title: titleRef.current?.innerText }, { at: path });
+    setRealTitle(titleRef.current?.innerText || defaultTitle);
+  }
 
   return (
-    <div {...attributes} >
-      <div  className={classnames(styles.callout, styles[calloutType])}>
-        <p contentEditable={false} className={styles.title}>{title || defaultTitle}</p>
-        {children}
+    <div>
+      <div className={classnames(styles.callout, styles[calloutType])}>
+        <div
+          contentEditable={false}
+          style={{ userSelect: 'none' }}
+        >
+          <p
+            data-slate-editor
+            ref={titleRef}
+            onBlur={handleTitleBlur}
+            className={styles.title}
+            contentEditable
+            suppressContentEditableWarning
+          >
+            {realTitle}
+          </p>
+        </div>
+        <div {...attributes}>
+          {children}
+        </div>
       </div>
       <AddParagraph element={element} />
     </div>
