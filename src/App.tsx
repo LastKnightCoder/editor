2fa7a712ec555726febaf43bc35209d265b6395a
lastknighcoder <2223106858@qq.com>
2023-10-24 09:31:24 +0800

    feat: 自动更新按钮

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,9 @@ import {RouterProvider} from "react-router-dom";
 
 import router from "@/router.tsx";
 import useTheme from "@/hooks/useTheme.ts";
-import useUpdate from "@/hooks/useUpdate.ts";
 
 const App = () => {
   const { isDark } = useTheme();
-  useUpdate();
 
   return (
     <ConfigProvider
--- src/layouts/DefaultLayout/Sidebar/index.tsx
+++ src/layouts/DefaultLayout/Sidebar/index.tsx
@@ -1,12 +1,16 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import classnames from "classnames";
 
-import { SettingOutlined } from '@ant-design/icons';
+import { message } from 'antd';
+import { SettingOutlined, SyncOutlined } from '@ant-design/icons';
 import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md';
 import IconText from "@/components/IconText";
 import useSettingStore from "@/stores/useSettingStore.ts";
 
 import styles from './index.module.less';
+import { checkUpdate, installUpdate } from '@tauri-apps/api/updater'
+import { relaunch } from '@tauri-apps/api/process'
+
 
 interface ISidebarProps {
   className?: string;
@@ -15,6 +19,8 @@ interface ISidebarProps {
 
 const Sidebar = (props: ISidebarProps) => {
   const { className, style } = props;
+
+  const [checking, setChecking] = useState(false);
 
   const {
     darkMode,
@@ -36,6 +42,58 @@ const Sidebar = (props: ISidebarProps) => {
     document.documentElement.dataset.theme = darkMode ? 'dark' : 'light';
   }, [darkMode]);
 
+  const check = async () => {
+    message.info('敬请期待');
+    return;
+
+    if (checking) return;
+    setChecking(true);
+    try {
+      // const res = await fetch('https://tauri-editor.oss-cn-hangzhou.aliyuncs.com/latest.json');
+      // const json = await res.json();
+      // const latestVersion = json.version;
+      // if (appVersion === latestVersion) {
+      //   message('有新版本');
+      //   const currentPlatform = await platform();
+      //   if (currentPlatform === 'darwin') {
+      //     const url = json.platforms['darwin-x86_64'].url;
+      //     message('有新版本: ' + url);
+      //   } else if (currentPlatform === 'linux') {
+      //     const url = json.platforms['linux-x86_64'].url;
+      //     message('有新版本: ' + url);
+      //   } else if (currentPlatform === 'win32') {
+      //     const url = json.platforms['windows-x86_64'].url;
+      //     message('有新版本: ' + url);
+      //   }
+      // } else {
+      //   message('已是最新版本');
+      // }
+      const { shouldUpdate, manifest } = await checkUpdate()
+
+      if (shouldUpdate) {
+        // You could show a dialog asking the user if they want to install the update here.
+        console.log(
+          `Installing update ${manifest?.version}, ${manifest?.date}, ${manifest?.body}`
+        )
+
+        // Install the update. This will also restart the app on Windows!
+        await installUpdate()
+
+        // On macOS and Linux you will need to restart the app manually.
+        // You could use this step to display another confirmation dialog.
+        await relaunch()
+      } else {
+        message.info('已是最新版本')
+      }
+    } catch (error) {
+      // @ts-ignore
+      message.error(error);
+      console.error(error)
+    } finally {
+      setChecking(false)
+    }
+  }
+
   return (
     <div className={classnames(styles.sidebar, className)} style={style}>
       <div>
@@ -46,6 +104,11 @@ const Sidebar = (props: ISidebarProps) => {
           icon={darkMode ? <MdOutlineLightMode /> : <MdOutlineDarkMode />}
           text={darkMode ? '浅色' : '深色'}
           onClick={toggleDarkMode}
+        />
+        <IconText
+          icon={<SyncOutlined className={checking ? styles.checking : ''} />}
+          text={'更新'}
+          onClick={check}
         />
         <IconText
           icon={<SettingOutlined />}
