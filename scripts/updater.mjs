47bef186f27edc8213892d3f3d7fe44fc4825bf7
lastknighcoder <2223106858@qq.com>
2023-10-25 02:31:25 +0800

    v0.2.15

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -1,6 +1,4 @@
-import fetch from 'node-fetch';
 import { getOctokit, context } from '@actions/github';
-import fs from 'fs';
 
 import updatelog from './updatelog.mjs';
 
@@ -35,81 +33,14 @@ async function updater() {
     tag: tag.name,
   });
 
-  // 需要生成的静态 json 文件数据，根据自己的需要进行调整
-  const updateData = {
-    version: tag.name,
-    // 使用 UPDATE_LOG.md，如果不需要版本更新日志，则将此字段置空
+  github.rest.repos.updateRelease({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
     notes: updatelog(tag.name),
-    pub_date: new Date().toISOString(),
-    platforms: {
-      win64: { signature: '', url: '' }, // compatible with older formats
-      linux: { signature: '', url: '' }, // compatible with older formats
-      darwin: { signature: '', url: '' }, // compatible with older formats
-      'darwin-aarch64': { signature: '', url: '' },
-      'darwin-x86_64': { signature: '', url: '' },
-      'linux-x86_64': { signature: '', url: '' },
-      'windows-x86_64': { signature: '', url: '' },
-      // 'windows-i686': { signature: '', url: '' }, // no supported
-    },
-  };
-
-  const setAsset = async (asset, reg, platforms) => {
-    let sig = '';
-    if (/.sig$/.test(asset.name)) {
-      sig = await getSignature(asset.browser_download_url);
-    }
-    platforms.forEach((platform) => {
-      if (reg.test(asset.name)) {
-        // 设置平台签名，检测应用更新需要验证签名
-        if (sig) {
-          updateData.platforms[platform].signature = sig;
-          return;
-        }
-        // 设置下载链接
-        updateData.platforms[platform].url = asset.browser_download_url;
-      }
-    });
-  };
-
-  const promises = latestRelease.assets.map(async (asset) => {
-    // windows
-    await setAsset(asset, /.msi.zip/, ['win64', 'windows-x86_64']);
-
-    // darwin
-    await setAsset(asset, /.app.tar.gz/, [
-      'darwin',
-      'darwin-x86_64',
-      'darwin-aarch64',
-    ]);
-
-    // linux
-    await setAsset(asset, /.AppImage.tar.gz/, ['linux', 'linux-x86_64']);
-  });
-  await Promise.allSettled(promises);
-
-  if (!fs.existsSync('updater')) {
-    fs.mkdirSync('updater');
-  }
-
-  // 将数据写入文件
-  fs.writeFileSync(
-    './updater/install.json',
-    JSON.stringify(updateData, null, 2)
-  );
-  console.log('Generate updater/install.json');
+    release_id: process.env.release_id,
+    draft: false,
+    prerelease: false
+  })
 }
 
 updater().catch(console.error);
-
-// 获取签名内容
-async function getSignature(url) {
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/octet-stream' },
-    });
-    return response.text();
-  } catch (_) {
-    return '';
-  }
-}
