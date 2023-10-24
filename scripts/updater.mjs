50a7d46feabc684c8bda44753c864960e301df06
lastknighcoder <2223106858@qq.com>
2023-10-25 02:52:21 +0800

    v0.2.18

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -1,6 +1,7 @@
 import { getOctokit, context } from '@actions/github';
 
 import updatelog from './updatelog.mjs';
+import pkg from '../package.json';
 
 const token = process.env.GITHUB_TOKEN;
 
@@ -10,33 +11,16 @@ async function updater() {
     process.exit(1);
   }
 
-  // 用户名，仓库名
-  const options = { owner: context.repo.owner, repo: context.repo.repo };
   const github = getOctokit(token);
 
-  // 获取 tag
-  const { data: tags } = await github.rest.repos.listTags({
-    ...options,
-    per_page: 10,
-    page: 1,
-  });
-
-  // 过滤包含 `v` 版本信息的 tag
-  const tag = tags.find((t) => t.name.startsWith('v'));
-  // console.log(`${JSON.stringify(tag, null, 2)}`);
-
-  if (!tag) return;
-
-  // 获取此 tag 的详细信息
-  const { data: latestRelease } = await github.rest.repos.getReleaseByTag({
-    ...options,
-    tag: tag.name,
-  });
+  const version = pkg.version;
+  const tag = `v${version}`;
+  const body = updatelog(tag);
 
   github.rest.repos.updateRelease({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    body: updatelog(tag.name),
+    body,
     release_id: process.env.release_id,
     draft: false,
     prerelease: false
