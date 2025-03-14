b1bc764627008b2ed23738114e1f19ab5e516cfe
LastKnightCoder <2223106858@qqcom>
2025-03-14 19:34:37 +0800

    feat: 添加代码质量工具配置，包括 ESLint、Prettier 和 Commitlint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,16 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     "@typescript-eslint/ban-ts-comment": "off",
     "object-curly-spacing": ["warn", "always"],
+    "prettier/prettier": "warn",
   },
 }
---
+++ .lintstagedrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
+  "*.{json,css,less,scss}": ["prettier --write"],
+  "*.{json,md}": ["prettier --write"],
+};
