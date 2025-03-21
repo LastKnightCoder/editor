84df5b9f6ba9743c5be207f41c1b781c744554c2
LastKnightCoder <2223106858@qq.com>
2025-03-21 10:55:22 +0800

    feat: callout 支持在不同的类型之间切换，主题适配优化

--- src/components/Editor/extensions/callout/components/Callout/index.tsx
+++ src/components/Editor/extensions/callout/components/Callout/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useMemo } from "react";
 import { Transforms } from "slate";
 import {
   RenderElementProps,
@@ -8,19 +8,24 @@ import {
 } from "slate-react";
 import classnames from "classnames";
 import { MdDragIndicator } from "react-icons/md";
+import { Dropdown } from "antd";
+import type { MenuProps } from "antd";
 
+import useTheme from "@/components/Editor/hooks/useTheme.ts";
 import useDragAndDrop from "@/components/Editor/hooks/useDragAndDrop.ts";
-import { CalloutElement } from "@/components/Editor/types";
+import { CalloutElement, CalloutType } from "@/components/Editor/types";
 import AddParagraph from "@/components/Editor/components/AddParagraph";
 
+import { DEFAULT_TITLE } from "../../constants.ts";
 import styles from "./index.module.less";
+import { useMemoizedFn } from "ahooks";
 
 interface ICalloutProps {
   attributes: RenderElementProps["attributes"];
   element: CalloutElement;
 }
 
-import { DEFAULT_TITLE } from "../../constants.ts";
+const trigger = ["click"];
 
 const Callout: React.FC<React.PropsWithChildren<ICalloutProps>> = (props) => {
   const { attributes, element, children } = props;
@@ -31,12 +36,13 @@ const Callout: React.FC<React.PropsWithChildren<ICalloutProps>> = (props) => {
   const [realTitle, setRealTitle] = useState<string>(title || defaultTitle);
   const titleRef = useRef<HTMLParagraphElement>(null);
 
+  const { isDark } = useTheme();
   const { drag, drop, isDragging, canDrag, canDrop, isBefore, isOverCurrent } =
     useDragAndDrop({
       element,
     });
 
-  const handleTitleBlur = () => {
+  const handleTitleBlur = useMemoizedFn(() => {
     const path = ReactEditor.findPath(editor, element);
     Transforms.setNodes(
       editor,
@@ -44,7 +50,50 @@ const Callout: React.FC<React.PropsWithChildren<ICalloutProps>> = (props) => {
       { at: path },
     );
     setRealTitle(titleRef.current?.innerText || defaultTitle);
-  };
+  });
+
+  const handleTransformCalloutType = useMemoizedFn((newType: CalloutType) => {
+    if (newType === calloutType) return;
+
+    const path = ReactEditor.findPath(editor, element);
+    Transforms.setNodes(
+      editor,
+      {
+        calloutType: newType,
+      },
+      { at: path },
+    );
+    if (realTitle === defaultTitle) {
+      setRealTitle(DEFAULT_TITLE[newType]);
+    }
+  });
+
+  const menu = useMemo(() => {
+    const calloutTypeItems: MenuProps["items"] = [
+      "tip",
+      "info",
+      "note",
+      "warning",
+      "danger",
+    ].map((type) => ({
+      key: type,
+      disabled: type === calloutType,
+      label: (
+        <div
+          className={classnames("callout-type-item", type, {
+            disabled: type === calloutType,
+          })}
+        >
+          <div className="color-indicator" />
+          {DEFAULT_TITLE[type as CalloutType]}
+        </div>
+      ),
+      onClick: () => handleTransformCalloutType(type as CalloutType),
+    }));
+    return {
+      items: calloutTypeItems,
+    };
+  }, [calloutType]);
 
   return (
     <div ref={drop} className={styles.container}>
@@ -55,6 +104,7 @@ const Callout: React.FC<React.PropsWithChildren<ICalloutProps>> = (props) => {
             [styles.drop]: isOverCurrent && canDrop,
             [styles.before]: isBefore,
             [styles.after]: !isBefore,
+            [styles.dark]: isDark,
           })}
         >
           <div contentEditable={false} style={{ userSelect: "none" }}>
@@ -72,16 +122,28 @@ const Callout: React.FC<React.PropsWithChildren<ICalloutProps>> = (props) => {
           </div>
           <div>{children}</div>
         </div>
-        <AddParagraph element={element} />
-        <div
-          contentEditable={false}
-          ref={drag}
-          className={classnames(styles.dragHandler, {
-            [styles.canDrag]: canDrag,
-          })}
-        >
-          <MdDragIndicator className={styles.icon} />
-        </div>
+        {!readOnly && <AddParagraph element={element} />}
+        {!readOnly && (
+          <Dropdown
+            menu={menu}
+            trigger={trigger as ["click"]}
+            disabled={readOnly}
+            overlayClassName={classnames("callout-type-dropdown", {
+              dark: isDark,
+            })}
+            placement="bottomRight"
+          >
+            <div
+              contentEditable={false}
+              ref={drag}
+              className={classnames(styles.dragHandler, {
+                [styles.canDrag]: canDrag,
+              })}
+            >
+              <MdDragIndicator className={styles.icon} />
+            </div>
+          </Dropdown>
+        )}
       </div>
     </div>
   );
