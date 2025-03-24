39b11d7861a3570007318e7f3cf04087a64b6bf6
LastKnightCoder <2223106858@qq.com>
2025-03-24 09:40:20 +0800

    fix: type error

--- src/editor-extensions/card-link/components/CardLink/index.tsx
+++ src/editor-extensions/card-link/components/CardLink/index.tsx
@@ -1,14 +1,11 @@
-import React, { useContext, useMemo } from "react";
+import React, { useMemo } from "react";
 import { Popover } from "antd";
 import { RenderElementProps } from "slate-react";
 
 import { CardLinkElement } from "@/editor-extensions/card-link";
 import InlineChromiumBugfix from "@/components/Editor/components/InlineChromiumBugFix";
 import CardContent from "../CardContent";
-import { EditCardContext } from "@/context";
-
 import useCardsManagementStore from "@/stores/useCardsManagementStore.ts";
-import useCardPanelStore, { EActiveSide } from "@/stores/useCardPanelStore";
 import useRightSidebarStore from "@/stores/useRightSidebarStore";
 
 import styles from "./index.module.less";
@@ -24,34 +21,13 @@ const CardLink = (props: ICardLinkProps) => {
   const { attributes, children, element } = props;
   const { cardId } = element;
 
-  const { cardId: editingCardId } = useContext(EditCardContext) || {};
-
   const { cards } = useCardsManagementStore((state) => ({
     cards: state.cards,
   }));
 
-  const { addCardToSide, leftCardIds, rightCardIds } = useCardPanelStore(
-    (state) => ({
-      addCardToSide: state.addCardToSide,
-      leftCardIds: state.leftCardIds,
-      rightCardIds: state.rightCardIds,
-    }),
-  );
-
   const linkCard = useMemo(() => {
     return cards.find((card) => card.id === cardId);
   }, [cards, cardId]);
-
-  const side = useMemo(() => {
-    if (!editingCardId) return EActiveSide.Left;
-    if (leftCardIds.includes(editingCardId)) {
-      return EActiveSide.Right;
-    } else if (rightCardIds.includes(editingCardId)) {
-      return EActiveSide.Left;
-    }
-
-    return EActiveSide.Left;
-  }, [leftCardIds, rightCardIds, editingCardId]);
 
   const handleCardClick = (e: React.MouseEvent) => {
     e.preventDefault();
